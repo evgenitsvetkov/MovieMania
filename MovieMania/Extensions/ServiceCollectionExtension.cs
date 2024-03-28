97b8d5de9526359c4001ba09ce71f0d531de46fa
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2024-03-29 00:12:30 +0200

    ServiceCollectionExtension for DbContext and Identity

--- MovieMania/Extensions/ServiceCollectionExtension.cs
+++ MovieMania/Extensions/ServiceCollectionExtension.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using MovieMania.Data;
+using MovieMania.Infrastructure.Data;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
