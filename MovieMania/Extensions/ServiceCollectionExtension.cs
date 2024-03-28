4d856cf7aa93ffe5c173c012818f9110911403bd
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2024-03-28 22:23:11 +0200

    MainMenu component added

--- MovieMania/Extensions/ServiceCollectionExtension.cs
+++ MovieMania/Extensions/ServiceCollectionExtension.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MovieMania.Data;
-using System.Runtime.CompilerServices;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
