e3a50849bde159d92db59c94bf8a64ca083bbf6c
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2024-12-12 23:07:07 +0200

    Integration Tests Added

---
+++ MovieMania.Tests/UnitTests/DirectorServiceTests.cs
@@ -0,0 +1,154 @@
+﻿using MovieMania.Core.Contracts;
+using MovieMania.Core.Models.Director;
+using MovieMania.Core.Services;
+using System.Globalization;
+
+namespace MovieMania.Tests.UnitTests
+{
+    public class DirectorServiceTests : UnitTestsBase
+    {
+        private IDirectorService directorService;
+
+        [OneTimeSetUp]
+        public void SetUp()
+           => directorService = new DirectorService(unitOfWork);
+
+        [Test]
+        public async Task AllAsync_ShouldReturnAllDirectors()
+        {
+            // Arrange
+
+            // Act
+            var directors = await directorService.AllAsync();
+
+            // Assert
+            Assert.That(directors.TotalDirectorsCount, Is.EqualTo(3));
+            Assert.IsInstanceOf<DirectorQueryServiceModel>(directors);
+        }
+
+        [Test]
+        public async Task ExistsAsync_ShouldReturnTrue_WithValidId()
+        {
+            // Arrange
+            var directorId = FirstDirector.Id;
+
+            // Act
+            var director = await directorService.ExistsAsync(directorId);
+
+            // Assert
+            Assert.IsTrue(director);
+        }
+
+        [Test]
+        public async Task ExistsAsync_ShouldReturnFalse_WithInvalidId()
+        {
+            // Arrange
+            var invalidId = 400;
+
+            // Act
+            var director = await directorService.ExistsAsync(invalidId);
+
+            // Assert
+            Assert.IsFalse(director);
+        }
+
+        [Test]
+        public async Task DirectorsDetailsByIdAsync_ShouldReturnCorrectDirector_WithValidId()
+        {
+            // Arrange
+            var directorId = FirstDirector.Id;
+
+            // Act
+            var director = await directorService.DirectorsDetailsByIdAsync(directorId);
+
+            // Assert
+            Assert.IsNotNull(director);
+            Assert.IsInstanceOf<DirectorDetailsServiceModel>(director);
+        }
+
+        [Test]
+        public async Task CreateAsync_ShouldCreateDirectorSuccessfully()
+        {
+            // Arrange
+            var directorFormModel = new DirectorFormModel() 
+            {
+                Name = "New Test Director Name",
+                Bio = "This is a description of the new director. This is a description of the new director. This is a description of the new director.",
+                BirthDate = DateTime.Now,
+                ImageUrl = ""
+            };
+
+            // Act
+            var directorId = await directorService.CreateAsync(directorFormModel);
+            var director = await directorService.ExistsAsync(directorId);
+
+            // Assert
+            Assert.IsTrue(director);
+        }
+
+        [Test]
+        public async Task GetDirectorFormModelByIdAsync_ShouldReturnCorrectDirectorFormModel_WithValidId()
+        {
+            // Arrange
+            var directorId = FirstDirector.Id;
+
+            // Act
+            var director = await directorService.GetDirectorFormModelByIdAsync(directorId);
+
+            // Assert
+            Assert.IsNotNull(director);
+            Assert.IsInstanceOf<DirectorFormModel>(director);
+        }
+
+        [Test]
+        public async Task GetDirectorFormModelByIdAsync_ShouldReturnNull_WithInvalidId()
+        {
+            // Arrange
+            var invalidId = 400;
+
+            // Act
+            var director = await directorService.GetDirectorFormModelByIdAsync(invalidId);
+
+            // Assert
+            Assert.IsNull(director);
+        }
+
+        [Test]
+        public async Task EditAsync_ShouldEditDirectorSuccessfully()
+        {
+            // Arrange
+            var directorId = FirstDirector.Id;
+
+            var directorFormModel = new DirectorFormModel()
+            {
+                Name = "Edited First Test Director",
+                Bio = "This is a edited test description. This is a edited test description. This is a edited test description.",
+                BirthDate = DateTime.ParseExact("20/10/1979", "20/10/1979", CultureInfo.InvariantCulture, DateTimeStyles.None),
+                ImageUrl = "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/8/8f/Anna_Boden.jpg/revision/latest?cb=20190305123354"
+            };
+
+            // Act
+            await directorService.EditAsync(directorId, directorFormModel);
+            var editedDirector = await directorService.GetDirectorFormModelByIdAsync(directorId);
+
+            // Assert
+            Assert.That(editedDirector.Name, Is.EqualTo(directorFormModel.Name));
+            Assert.That(editedDirector.Bio, Is.EqualTo(directorFormModel.Bio));
+        }
+
+        [Test]
+        public async Task DeleteAsync_ShouldDeleteDirectorSuccessfully()
+        {
+            // Arrange
+            var directorId = SecondDirector.Id;
+
+            // Act
+            await directorService.DeleteAsync(directorId);
+            var director = await directorService.ExistsAsync(directorId);
+
+            // Assert
+            Assert.IsFalse(director);
+        }
+
+    }
+}
---
+++ MovieMania.Tests/UnitTests/MovieServiceTests.cs
@@ -0,0 +1,288 @@
+﻿using MovieMania.Core.Contracts;
+using MovieMania.Core.Models.Movie;
+using MovieMania.Core.Services;
+using NUnit.Framework.Internal;
+
+namespace MovieMania.Tests.UnitTests
+{
+    [TestFixture]
+    public class MovieServiceTests : UnitTestsBase
+    {
+        private IMovieService movieService;
+
+        [OneTimeSetUp]
+        public void SetUp()
+            => movieService = new MovieService(unitOfWork);
+
+        [Test]
+        public async Task ExistsAsync_ShouldReturnTrue_WithValidId()
+        {
+            // Arrange
+            var movieId = FifthMovie.Id;
+
+            // Act
+            var movie = await movieService.ExistsAsync(movieId);
+
+            // Assert
+            Assert.IsTrue(movie);
+        }
+
+        [Test]
+        public async Task ExistsAsync_ShouldReturnFalse_WithInvalidId()
+        {
+            // Arrange
+            var invalidId = 300;
+
+            // Act
+            var movie = await movieService.ExistsAsync(invalidId);
+
+            // Assert
+            Assert.IsFalse(movie);
+        }
+
+        [Test]
+        public async Task AllGenresAsync_ShouldReturnAllGenres()
+        {
+            // Arrange
+
+            // Act
+            var genres = await movieService.AllGenresAsync();
+
+            // Assert
+            Assert.IsTrue(genres.Any());
+        }
+
+        [Test]
+        public async Task AllDirectorsAsync_ShouldReturnAllDirectors()
+        {
+            // Arrange
+
+            // Act
+            var directors = await movieService.AllDirectorsAsync();
+
+            // Assert
+            Assert.IsTrue(directors.Any());
+        }
+
+        [Test]
+        public async Task GenreExistsAsync_ShouldReturnTrue_WithValidId()
+        {
+            // Arrange 
+            var genreId = FirstGenre.Id;
+
+            // Act
+            var genre = await movieService.GenreExistsAsync(genreId);
+
+            // Assert
+            Assert.IsTrue(genre);
+        }
+
+        [Test]
+        public async Task GenreExistsAsync_ShouldReturnFalse_WithInvalidId()
+        {
+            // Arrange 
+            var invalidId = 300;
+
+            // Act
+            var genre = await movieService.GenreExistsAsync(invalidId);
+
+            // Assert
+            Assert.IsFalse(genre);
+        }
+
+        [Test]
+        public async Task DirectorExistsAsync_ShouldReturnTrue_WithValidId()
+        {
+            // Arrange
+            var directorId = FirstDirector.Id;
+
+            // Act
+            var director = await movieService.DirectorExistsAsync(directorId);
+
+            // Assert
+            Assert.IsTrue(director);
+        }
+
+        [Test]
+        public async Task DirectorExistsAsync_ShouldReturnFalse_WithInvalidId()
+        {
+            // Arrange
+            var invalidId = 300;
+
+            // Act
+            var director = await movieService.DirectorExistsAsync(invalidId);
+
+            // Assert
+            Assert.IsFalse(director);
+        }
+
+        [Test]
+        public async Task CreateAsync_ShouldCreateMovieSuccessfully()
+        {
+            // Arrange
+            var movieFormModel = new MovieFormModel() 
+            {
+                Title = "New Test Movie Title",
+                GenreId = 2,
+                DirectorId = 2,
+                ReleaseDate = 2001,
+                Price = 29.99M,
+                Description = "This is test description for the new movie. This is test description for the new movie. This is test description for the new movie.",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/en/b/b8/Just_Go_with_It_Poster.jpg"
+            };
+
+            //Act 
+            var newMovieId = await movieService.CreateAsync(movieFormModel);
+            var movieModel = await movieService.MoviesDetailsByIdAsync(newMovieId);
+
+            // Assert
+            Assert.IsNotNull(movieModel);
+            Assert.That(newMovieId, Is.EqualTo(movieModel.Id));
+        }
+
+        [Test]
+        public async Task AllAsync_ShouldReturnAllMovies()
+        {
+            // Arrange
+
+            // Act
+            var movies = await movieService.AllAsync();
+
+            // Assert
+            Assert.IsInstanceOf<MovieQueryServiceModel>(movies);
+            Assert.That(movies.TotalMoviesCount, Is.EqualTo(6));
+        }
+
+        [Test]
+        public async Task AllAsync_ShouldReturnAllMovies_WithCorrectGenre()
+        {
+            // Arrange
+            string firstGenreName = FirstGenre.Name;
+
+            // Act
+            var movies = await movieService.AllAsync(firstGenreName);
+            var movieGenre = movies.Movies.FirstOrDefault(c => c.Genre == firstGenreName);
+
+            // Assert
+            Assert.That(movies.TotalMoviesCount, Is.EqualTo(2));
+            Assert.That(movieGenre.Genre, Is.EqualTo(firstGenreName));
+        }
+
+        [Test]
+        public async Task AllAsync_ShouldReturnEmpty_WithInvalidGenre()
+        {
+            // Arrange
+            string invalidGenre = "Invalid Genre";
+
+            // Act
+            var movies = await movieService.AllAsync(invalidGenre);
+            
+            // Assert
+            Assert.That(movies.TotalMoviesCount, Is.EqualTo(0));
+            Assert.That(movies.Movies, Is.Empty);
+        }
+
+        [Test]
+        public async Task AllGenresNamesAsync_ShouldReturnAllGenresNames()
+        {
+            // Arrange
+
+            // Act
+            var genreNames = await movieService.AllGenresNamesAsync();
+
+            // Assert
+            Assert.IsTrue(genreNames.Any());
+        }
+
+        [Test]
+        public async Task MoviesDetailsByIdAsync_ShouldReturnMovieDetails_WithValidId()
+        {
+            // Arrange
+            var movieId = ThirdMovie.Id;
+
+            // Act 
+            var movie = await movieService.MoviesDetailsByIdAsync(movieId);
+
+            // Assert
+            Assert.That(movie.Id, Is.EqualTo(movieId));
+            Assert.IsInstanceOf<MovieDetailsServiceModel>(movie);
+        }
+
+        [Test]
+        public async Task EditAsync_ShouldEditMovieSuccessfully()
+        {
+            // Arrange
+            var newMovieFormModel = new MovieFormModel()
+            {
+                Title = "New Edited Test Movie Title",
+                GenreId = 1,
+                DirectorId = 1,
+                ReleaseDate = 2011,
+                Price = 29.99M,
+                Description = "This is a edited test description. This is a edited test description. This is a edited test description.",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/en/b/b8/Just_Go_with_It_Poster.jpg"
+            };
+
+            // Act
+            await movieService.EditAsync(SixthMovie.Id, newMovieFormModel);
+
+            // Assert
+            Assert.That(SixthMovie.Title, Is.EqualTo(newMovieFormModel.Title));
+        }
+
+        [Test]
+        public async Task GetMovieFormModelByIdAsync_ShouldReturnCorrectMovieFormModel_WithValidId()
+        {
+            // Arrange
+            var movieId = SecondMovie.Id;
+            var secondMovieTitle = SecondMovie.Title;
+
+            // Act
+            var movie = await movieService.GetMovieFormModelByIdAsync(movieId);
+
+            // Assert
+            Assert.IsNotNull(movie);
+            Assert.IsInstanceOf<MovieFormModel>(movie);
+            Assert.That(movie.Title, Is.EqualTo(secondMovieTitle));
+        }
+
+        [Test]
+        public async Task GetMovieFormModelByIdAsync_ShouldReturnNull_WithInvalidId()
+        {
+            // Arrange
+            var invalidId = 300;
+
+            // Act
+            var movie = await movieService.GetMovieFormModelByIdAsync(invalidId);
+
+            // Assert
+            Assert.IsNull(movie);
+        }
+
+        [Test]
+        public async Task DeleteAsync_ShouldDeleteMovieSuccessfully()
+        {
+            // Arrange
+            var movieId = FirstMovie.Id;
+
+            // Act
+            await movieService.DeleteAsync(movieId);
+            var movie = await movieService.ExistsAsync(movieId);
+
+            // Assert
+            Assert.IsFalse(movie);
+        }
+
+        [Test]
+        public async Task LastFiveMoviesAsync_ShouldReturnCorrectMovies()
+        {
+            // Arrange
+
+            // Act
+            var movies = await movieService.LastFiveMoviesAsync();
+
+            //Assert
+            Assert.That(movies.Count(), Is.EqualTo(5));
+        }
+    }
+}
